ea3510cebe3e2bb2efd9fbc8f0e6350041a4dfc4
Oscar91-web <56107445+Oscar91-web@users.noreply.github.com>
2020-05-13 21:22:27 +0200

    Updates

---
+++ precache-manifest.99d19f50a269145b72973f542d464d82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "157c969fc328c60620127009c8937da9",
+    "url": "/starwarsReact/index.html"
+  },
+  {
+    "revision": "03857b7d24d5709cd136",
+    "url": "/starwarsReact/static/css/main.cebad928.chunk.css"
+  },
+  {
+    "revision": "3af419abb4f774eeac07",
+    "url": "/starwarsReact/static/js/2.fd69bbe8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/starwarsReact/static/js/2.fd69bbe8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03857b7d24d5709cd136",
+    "url": "/starwarsReact/static/js/main.93391bae.chunk.js"
+  },
+  {
+    "revision": "8b838a23803ffe84a426",
+    "url": "/starwarsReact/static/js/runtime-main.0ff5f149.js"
+  }
+]);
